5b6a7e576a8c4472f07334629cf75c06d30406e6
Thomas Zangerle <thomas.zangerle@icloud.com>
2018-08-09 20:18:29 +0200

    Graphics with births box plots vs. weekday and full moon data.

--- src/eda.R
+++ src/eda.R
@@ -7,10 +7,16 @@ births <- read_csv("results/moon-births.csv")
 levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
 births <- births %>% 
-  mutate(weekday = factor(weekdays(date), levels = levels))
-
-ggplot(births, aes(date, num_births)) +
-  geom_point()
+  mutate(
+    weekday = factor(weekdays(date), levels = levels),
+    full_moon = (!is.na(phaseid)&(phaseid == 4))
+  )
 
 ggplot(births, aes(weekday, num_births)) + 
-  geom_boxplot()
+  geom_boxplot(aes(color = full_moon)) +
+  labs(title = "Births count vs. weekday and full moon")
+
+ggsave('results/births_weekday_moon.png')
+
+count(births, weekday, full_moon)
+
